b1763f8ba40f1cfd26dd59b1f85950f2b172423b
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2020-12-16 12:26:07 -0800

    initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+<a href="https://myoctocat.dev/@gr2m/octoherd/"><img alt="Octoherd Avatar" src="https://raw.githubusercontent.com/octoherd/cli/main/assets/octoherd.gif" width=300 height=300 align=left></a>
+
+# Octoherd scripts
+
+See the [scripts/](scripts/) folder
+
+## License
+
+[ISC](LICENSE)
---
+++ scripts/remove-dependabot/script.js
@@ -0,0 +1,51 @@
+module.exports.script = removeDependabot;
+
+/**
+ * Create a CODE_OF_CONDUCT.md file unless it already exists.
+ * Ignores forks and archived repositories
+ *
+ * @param {import('@octokit/core').Octokit} octokit
+ * @param {import('@octokit/openapi-types').components["schemas"]["repository"]} repository
+ */
+async function removeDependabot(octokit, repository) {
+  if (repository.archived) {
+    octokit.log.info(`${repository.html_url} is archived, ignoring.`);
+    return;
+  }
+
+  const owner = repository.owner.login;
+  const repo = repository.name;
+  const path = ".github/dependabot.yml";
+
+  const sha = await octokit
+    .request("GET /repos/{owner}/{repo}/contents/{path}", {
+      owner,
+      repo,
+      path,
+    })
+    .then(
+      (response) => response.data.sha,
+      (error) => null
+    );
+
+  if (!sha) {
+    octokit.log.info(`${path} does not exist in ${repository.html_url}`);
+    return;
+  }
+
+  const {
+    data: { commit },
+  } = await octokit.request("DELETE /repos/{owner}/{repo}/contents/{path}", {
+    owner,
+    repo,
+    path,
+    sha,
+    message: `build(dependabot): disable by removing configuration
+    
+I'm taking a break. I'll enable automated dependency updates when I'm back in 2021. But probably not with Dependabot, but Renovate.`,
+  });
+
+  octokit.log.info(
+    `${path} deleted in ${repository.html_url} via ${commit.html_url}`
+  );
+}
