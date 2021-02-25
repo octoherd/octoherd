1f4ccaefe6df0cb35efc5d2f247188c5019b4a17
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-02-25 13:47:31 -0800

    feat: add find-releases scripts

---
+++ scripts/find-releases/script.js
@@ -0,0 +1,44 @@
+/**
+ * Find releases in repositories. Pass an optional `since` option (`YYYY-MM-dd`) to ignore
+ * releases prior a set date
+ *
+ * @param {import('@octoherd/octokit').Octokit} octokit
+ * @param {import('@octokit/openapi-types').components["schemas"]["repository"]} repository
+ * @param { {since?: string} } options Custom user options passed to the CLI
+ */
+export async function script(octokit, repository, options) {
+  const since = options.since || "";
+
+  const owner = repository.owner.login;
+  const repo = repository.name;
+
+  // https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#list-releases
+  const allReleases = await octokit.paginate(
+    "GET /repos/{owner}/{repo}/releases",
+    {
+      owner,
+      repo,
+      per_page: 100,
+    }
+  );
+
+  const releases = allReleases.filter((release) => {
+    return since ? release.created_at > String(since) : true;
+  });
+
+  octokit.log.info(
+    {
+      releases: releases.map((release) => {
+        return {
+          created_at: release.created_at,
+          version: release.tag_name,
+          notes: release.body,
+        };
+      }),
+      repository: [owner, repo].join("/"),
+    },
+    `${releases.length} releases found in ${repository.html_url}${
+      since ? ` since ${since}` : ""
+    }`
+  );
+}
